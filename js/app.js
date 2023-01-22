a0085792fc100f5c02ae5b3715efa137ba8a5785
Ryan Silva de Menezes <85131927+RyanSilva-176@users.noreply.github.com>
2023-01-22 20:54:22 -0300

    Add - JS

--- js/app.js
+++ js/app.js
@@ -1,37 +1,37 @@
-const chk = document.getElementById('chk')
-var box = document.getElementsByClassName('box')
-var heading = document.getElementsByClassName('heading')
-var heading_md = document.getElementsByClassName('heading-md')
-var text = document.getElementsByClassName('text')
-var logo = document.getElementsByClassName('logo-name')
-var icons = document.getElementsByClassName('fa-solid')
+const chk = document.getElementById("chk");
+var box = document.getElementsByClassName("box");
+var heading = document.getElementsByClassName("heading");
+var heading_md = document.getElementsByClassName("heading-md");
+var text = document.getElementsByClassName("text");
+var logo = document.getElementsByClassName("logo-name");
+var icons = document.getElementsByClassName("fa-solid");
 let buttons = document.querySelector(".buttons");
 let btn = buttons.querySelectorAll(".btn");
 
-chk.addEventListener('change', () => {
-  document.body.classList.toggle('light')
-  for (var x=0;x<box.length;x++) {
-    box[x].classList.toggle('light')
+chk.addEventListener("change", () => {
+  document.body.classList.toggle("light");
+  for (var x = 0; x < box.length; x++) {
+    box[x].classList.toggle("light");
   }
-  for (var x=0;x<text.length;x++) {
-    text[x].classList.toggle('light')
+  for (var x = 0; x < text.length; x++) {
+    text[x].classList.toggle("light");
   }
-  for (var x=0;x<heading_md.length;x++) {
-    heading_md[x].classList.toggle('light')
+  for (var x = 0; x < heading_md.length; x++) {
+    heading_md[x].classList.toggle("light");
   }
-  for (var x=0;x<heading.length;x++) {
-    heading[x].classList.toggle('light')
+  for (var x = 0; x < heading.length; x++) {
+    heading[x].classList.toggle("light");
   }
-  for (var x=0;x<logo.length;x++) {
-    logo[x].classList.toggle('light')
+  for (var x = 0; x < logo.length; x++) {
+    logo[x].classList.toggle("light");
   }
-  for (var x=0;x<icons.length;x++) {
-    icons[x].classList.toggle('light')
+  for (var x = 0; x < icons.length; x++) {
+    icons[x].classList.toggle("light");
   }
-  for (var x=0;x<btn.length;x++) {
-    btn[x].classList.toggle('light')
+  for (var x = 0; x < btn.length; x++) {
+    btn[x].classList.toggle("light");
   }
-})
+});
 
 for (var i = 0; i < btn.length; i++) {
   btn[i].addEventListener("click", function () {
@@ -40,3 +40,15 @@ for (var i = 0; i < btn.length; i++) {
     this.className += " active";
   });
 }
+
+function msgAlert() {
+  let msgAlert = document.querySelector("#msg-alert");
+  msgAlert.setAttribute("style", "visibility: visible");
+}
+
+function closeOpt() {
+  let msgAlert = document.querySelector("#msg-alert");
+  msgAlert.setAttribute("style", "visibility: hidden");
+}
+
+
